8615c33459a16684e90c90c3ada4e23cf3c77328
rymc1337 <dafinqinthara@gmail.com>
2021-11-10 18:20:29 +0700

    First Commit

---
+++ Assets/Sound/Testing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+
+    public List<AudioClip> clips;
+    public AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+    }
+
+    public void Play(int clickNum)
+    {
+        source.PlayOneShot(clips[clickNum]);
+    }
+}
+
+   
+
---
+++ Assets/Sound/TriggerSFX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSFX : MonoBehaviour
+{
+    public AudioSource playsound;
+
+
+    void Start()
+    {
+        playsound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void test()
+    {
+        playsound.Play();
+    }
+
+}
